4741781717914dc8b04d4dcdc8f3efa2685276ee
Jxx5o2 <vtsun98@gmail.com>
2025-07-10 15:53:13 +0900

    레이저 두갈래 + 조준점 구현
    배열 이용해서 레이저 오브젝트 2개 받고 foreach문으로 레이저 두 개 받아서 기존 발사 코드에 적용, UI image 이용하고 이미지 파일이 마우스 위치에 올 수 있도록 구현

--- Galaxy Strike/Assets/Scripts/PlayerWeapon.cs
+++ Galaxy Strike/Assets/Scripts/PlayerWeapon.cs
@@ -1,16 +1,41 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerWeapon : MonoBehaviour
 {
-    
+    [SerializeField] GameObject[] lasers;
+    [SerializeField] RectTransform crosshair;
+
+    bool isFiring = false;
+
     void Start()
     {
-        
+        Cursor.visible = false;
     }
 
-    
     void Update()
     {
-        
+        ProcessFiring();
+        MoveCrosshair();
+    }
+
+    public void OnFire(InputValue value)
+    {
+        isFiring = value.isPressed;
+    }
+
+    void ProcessFiring()
+    {
+        foreach (GameObject laser in lasers)
+        {
+            var emmissionModule = laser.GetComponent<ParticleSystem>().emission;
+            emmissionModule.enabled = isFiring;
+        }
+
+    }
+
+    void MoveCrosshair()
+    {
+        crosshair.position = Input.mousePosition;
     }
 }
