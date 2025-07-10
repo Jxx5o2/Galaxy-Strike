1fa4f64336b8b5beb2dea211ade6c681e5dbec2a
Jxx5o2 <vtsun98@gmail.com>
2025-07-10 14:44:50 +0900

    비행기 이동  및, 이동시 회전 구현, + 레이저 구현
    Mathf.Clamp로 이동 제한 걸고, Quaternion.Lerp, Quaternion.Euler로 회전 구현 후 선형보간 해서 자연스럽게 회전 하도록 만듬.
    추가로 파티클 설정해서 공격 이펙트 구현함

--- Galaxy Strike/Assets/Scripts/PlayerMovement.cs
+++ Galaxy Strike/Assets/Scripts/PlayerMovement.cs
@@ -5,20 +5,46 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float controlSpeed = 10f;
+    [SerializeField] float xClampRange = 5f;
+    [SerializeField] float yClampRange = 5f;
+
+    [SerializeField] float controlRollFactor = 45f;
+    [SerializeField] float controlPitchFactor = 45f;
+
+    [SerializeField] float rotationSpeed = 10f;
 
     Vector2 movement;
     void Update()
     {
-        float xOffset = movement.x * controlSpeed * Time.deltaTime;
-        float yOffset = movement.y * controlSpeed * Time.deltaTime;
-
-        transform.localPosition
-        = new Vector3(transform.localPosition.x + xOffset, transform.localPosition.y + yOffset, 0f);
-
+        ProcessTranslation();
+        ProcessRotation();
     }
 
     public void OnMove(InputValue value)
     {
         movement = value.Get<Vector2>();
+    }
+
+    private void ProcessTranslation()
+    {
+        float xOffset = movement.x * controlSpeed * Time.deltaTime;
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xClampRange, xClampRange);
+
+        float yOffset = movement.y * controlSpeed * Time.deltaTime;
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, -yClampRange, yClampRange);
+
+        transform.localPosition
+        = new Vector3(clampedXPos, clampedYPos, 0f);
+    }
+
+    void ProcessRotation()
+    {
+        float pitch = -controlPitchFactor * movement.y;
+        float roll = -controlRollFactor * movement.x;
+        Quaternion targetRotation = Quaternion.Euler(pitch, 0f, roll);
+        transform.localRotation
+        = Quaternion.Lerp(transform.localRotation, targetRotation, rotationSpeed * Time.deltaTime);
     }
 }
---
+++ Galaxy Strike/Assets/Scripts/PlayerWeapon.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PlayerWeapon : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
