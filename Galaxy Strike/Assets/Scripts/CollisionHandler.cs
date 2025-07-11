0b3b037b1332b33607f5d6f375b33de3afef49d6
Jxx5o2 <vtsun98@gmail.com>
2025-07-11 16:28:47 +0900

    배경음악 + 레벨 리로딩 구현

--- Galaxy Strike/Assets/Scripts/CollisionHandler.cs
+++ Galaxy Strike/Assets/Scripts/CollisionHandler.cs
@@ -3,8 +3,17 @@ using UnityEngine;
 public class CollisionHandler : MonoBehaviour
 {
     [SerializeField] GameObject destroyedVFX;
+
+    GameSceneManager gameSceneManager;
+
+    void Start()
+    {
+        gameSceneManager = FindFirstObjectByType<GameSceneManager>();
+    }
+
     void OnTriggerEnter(Collider other)
     {
+        gameSceneManager.ReloadLevel();
         Instantiate(destroyedVFX, transform.position, Quaternion.identity);
         Destroy(this.gameObject);
     }
